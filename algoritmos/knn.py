f9078e4d982298a5168ce41025d0c62710a2341f
Pedro Vitor <Pedrovsd00@gmail.com>
2026-04-28 15:23:25 -0300

    Adicionando a funçao de similaridade de cosseno

--- algoritmos/knn.py
+++ algoritmos/knn.py
@@ -5,13 +5,29 @@ from importdataset import carregar_dataset_rating
 dataset_notas = carregar_dataset_rating()
 matriz_notas = np.array(dataset_notas)
 
+notas = matriz_notas[:, 2]
+usuarios = matriz_notas[:, 0]
+
+mascara_notas = (notas > 0)
+# O que fazer com os valores faltantes??
+usuarios_com_notas = usuarios[mascara_notas]
+
+def similaridade(vetor_X, vetor_Y):
+    vetor_X = notas
+    vetor_Y = usuarios
+
+    produto_vetores = np.dot(vetor_X, vetor_Y)
+
+    norma_vetor_X = np.linalg.norm(vetor_X)
+    norma_vetor_Y = np.linalg.norm(vetor_Y)
+
+    if norma_vetor_X == 0 and norma_vetor_Y == 0:
+        return 0
+
+    return produto_vetores / (norma_vetor_X * norma_vetor_Y)
+
 def knn_user_based():
-    notas = matriz_notas[:, 2]
-    usuarios = matriz_notas[:, 0]
 
-    mascara_notas = (notas > 0)
-
-    usuarios_com_notas = usuarios[mascara_notas]
 
     return len(usuarios_com_notas)
 
